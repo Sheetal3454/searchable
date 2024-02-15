5ec155e74f63474d1324e9bdfebdfbfc4a6eb47b
Sheetal3454 <65387368+Sheetal3454@users.noreply.github.com>
2024-02-16 00:47:05 +0545

    Second Commit

---
+++ Searchable/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Searchable.Data;
+using Searchable.Models;
+
+namespace Searchable.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly TaskDbContext context;
+
+        public CategoryController(TaskDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+
+            var catData = context.Categories.ToList();
+
+            return View(catData);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                var categ = new Category()
+                {
+                    CategoryName = model.CategoryName,
+                    CreatedBy = model.CreatedBy,
+                    CreatedDate = model.CreatedDate,
+                    ModifiedBy = model.ModifiedBy,
+                    ModifiedDate = model.ModifiedDate,
+
+                };
+                context.Categories.Add(categ);
+                context.SaveChanges();
+                TempData["error"] = "Record Saved";
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                TempData["message"] = "Empty field cannot Submit";
+                return View(model);
+            }
+
+
+        }
+        public IActionResult Delete(int id)
+        {
+            var categ = context.Categories.SingleOrDefault(d => d.CategoryId == id);
+            context.Categories.Remove(categ);
+            context.SaveChanges();
+            TempData["error"] = "Record Deleted";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var categ = context.Categories.SingleOrDefault(c => c.CategoryId == id);
+            var result = new Category()
+            {
+                CategoryName = categ.CategoryName,
+                CreatedBy = categ.CreatedBy,
+                CreatedDate = categ.CreatedDate,
+                ModifiedBy = categ.ModifiedBy,
+                ModifiedDate = categ.ModifiedDate,
+
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category model)
+        {
+            var categ = new Category()
+            {
+                CategoryId = model.CategoryId,
+                CategoryName = model.CategoryName,
+                CreatedBy = model.CreatedBy,
+                CreatedDate = model.CreatedDate,
+                ModifiedBy = model.ModifiedBy,
+                ModifiedDate = model.ModifiedDate,
+            };
+            context.Categories.Update(categ);
+            context.SaveChanges();
+            TempData["error"] = "Record updated";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Searchable/Controllers/TasskController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Searchable.Data;
+using Searchable.Models;
+using Searchable.Models.ViewModel;
+
+namespace Searchable.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskDbContext context;
+
+        public TaskController(TaskDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+
+            var data = from t in context.Tassks
+                       join u in context.Users on t.UserId equals u.UserId
+                       join c in context.Categories on t.CategoryId equals c.CategoryId
+                       select new TasskSummaryView
+                       {
+                           TaskId = t.TaskId,
+                           Title = t.Title,
+                           Description = t.Description,
+                           DueDate = t.DueDate,
+                           IsCompleted = t.IsCompleted,
+                           UserId = u.UserId,
+
+                           CategoryId = c.CategoryId,
+
+                       };
+
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Tassk model)
+        {
+            if (ModelState.IsValid)
+            {
+                var tas = new Tassk()
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    DueDate = model.DueDate,
+                    IsCompleted = model.IsCompleted,
+                    UserId = model.UserId,
+                    CategoryId = model.CategoryId,
+
+                };
+                context.Tasks.Add(tas);
+                context.SaveChanges();
+                TempData["error"] = "Record Saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Empty Field cannpt Submitted";
+                return View(model);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tas = context.Tasks.SingleOrDefault(b => b.TaskId == id);
+            context.Tasks.Remove(tas);
+            context.SaveChanges();
+            TempData["error"] = "Record Deleted";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var tas = context.Tasks.SingleOrDefault(b => b.TaskId == id);
+            var result = new Tassk()
+            {
+                Title = tas.Title,
+                Description = tas.Description,
+                DueDate = tas.DueDate,
+                IsCompleted = tas.IsCompleted,
+                UserId = tas.UserId,
+                CategoryId = tas.CategoryId,
+
+
+            };
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(Tassk model)
+        {
+            var tas = new Tassk()
+            {
+                TaskId = model.TaskId,
+                Title = model.Title,
+                Description = model.Description,
+                DueDate = model.DueDate,
+                IsCompleted = model.IsCompleted,
+                UserId = model.UserId,
+                CategoryId = model.CategoryId,
+
+
+            };
+            context.Tasks.Update(tas);
+            context.SaveChanges();
+            TempData["error"] = "Record updated";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
